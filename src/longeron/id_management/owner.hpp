4cb97ab80fa257d2c99bbc1a1ad630053c2300bd
Neal_Nicdao <chrisnicdao0@gmail.com>
2022-04-29 16:35:30 -0700

    Rename IdStorage to IdOwner

--- src/longeron/id_management/owner.hpp
+++ src/longeron/id_management/owner.hpp
@@ -25,29 +25,29 @@ namespace lgrn
  * in debug. This class should be identical to a raw ID type on release.
  */
 template<typename ID_T, typename REG_T>
-class IdStorage
+class IdOwner
 {
     friend REG_T;
 
 public:
-    IdStorage() : m_id{ id_null<ID_T>() } { }
-    IdStorage(IdStorage&& move)
+    IdOwner() : m_id{ id_null<ID_T>() } { }
+    IdOwner(IdOwner&& move)
      : m_id{std::exchange(move.m_id, id_null<ID_T>())}
     { }
-    ~IdStorage()
+    ~IdOwner()
     {
-        LGRN_ASSERTMV( ! has_value(), "IdStorage's value must be cleared by its friend class before destruction",
+        LGRN_ASSERTMV( ! has_value(), "IdOwner's value must be cleared by its friend class before destruction",
                       std::size_t(m_id));
     }
 
     // Delete copy
-    IdStorage(IdStorage const& copy) = delete;
-    IdStorage& operator=(IdStorage const& copy) = delete;
+    IdOwner(IdOwner const& copy) = delete;
+    IdOwner& operator=(IdOwner const& copy) = delete;
 
     // Allow move assignment only if there's no ID stored
-    IdStorage& operator=(IdStorage&& move)
+    IdOwner& operator=(IdOwner&& move)
     {
-        LGRN_ASSERTMV( ! has_value(), "IdStorage's value must be cleared by its friend class before replacing stored value",
+        LGRN_ASSERTMV( ! has_value(), "IdOwner's value must be cleared by its friend class before replacing stored value",
                       std::size_t(m_id), std::size_t(move.m_id));
         m_id = std::exchange(move.m_id, id_null<ID_T>());
         return *this;
@@ -58,10 +58,10 @@ public:
     constexpr bool has_value() const noexcept { return m_id != id_null<ID_T>(); }
 
 private:
-    IdStorage(ID_T id) : m_id{ id } { }
+    IdOwner(ID_T id) : m_id{ id } { }
 
     ID_T m_id;
 
-}; // class IdStorage
+}; // class IdOwner
 
 } // namespace lgrn
